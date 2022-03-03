41825d6c638fd72fddab41683bc8eb7a3b2a93e3
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2022-03-03 19:59:52 +0700

    beta test

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    extend: {
+        container: {
+            center: true,
+            padding: '1rem'
+        }
+    },
+  },
+  plugins: [],
+}
